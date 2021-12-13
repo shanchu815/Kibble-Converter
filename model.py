e62fca355aa9bfc88a176100891dfb223bb9551d
Shannon <shanchu815@gmail.com>
2021-12-13 23:18:01 +0000

    subtitle changed to ingredient data, edited model.py to be clearer

--- model.py
+++ model.py
@@ -19,7 +19,37 @@ class Food(db.Model):
     def __repr__(self):
         return f'<Food product name={self.product_name} food id={self.food_id}>'
 
+class BrandFood(db.Model):
+    """A specific pet food that belongs to this particular brand"""
+
+    __tablename__ = 'brand_foods'
+
+    brand_food_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    food_id = db.Column(db.Integer,
+                        db.ForeignKey('foods.food_id'))
+    ingredient_id = db.Column(db.Integer,
+                        db.ForeignKey('brands.brand_id'))
+
+class Brand(db.Model):
+    """A brand of pet food."""
+
+    __tablename__ = 'brands'
+
+    brand_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    brand_name = db.Column(db.String)
+    food_id = db.Column(db.Integer,
+                        db.ForeignKey('foods.food_id'))
+
+    def __repr__(self):
+        return f'<Brand name={self.name} brand id={self.brand_id}>'
+
 class FoodIngredient(db.Model):
+    """An ingredient that's in this specific food"""
+
     __tablename__ = 'food_ingredients'
 
     food_ingredient_id = db.Column(db.Integer,
@@ -41,53 +71,55 @@ class Ingredient(db.Model):
     ingredient_name = db.Column(db.String)
     food_id = db.Column(db.Integer,
                         db.ForeignKey('foods.food_id'))
-    filler = db.Column(db.Boolean) # True if this is a filler else false
-    title = db.Column(db.String)
+    obscure_name = db.Column(db.Boolean) # True if this ingredient is of concern (like a chemical), else false
+    descriptor = db.Column(db.String)
     min_percentage = db.Column(db.Float)
     max_percentage = db.Column(db.Float)
 
-
-    # title_id = db.Column(db.Integer,i
-    #                     autoincrement=True)
-    # sub_id = db.Column(db.Integer,
-    #                     autoincrement=True)
- 
-
-    # fillers = db.relationship('Filler', backref='ingredients')
-    # title_ingredients = db.relationship('TitleIngredient', backref='ingredients')
-    subtitle_ingredients = db.relationship('SubtitleIngredient', backref='ingredients')
+    ingredient_data = db.relationship('IngredientData', backref='ingredients')
 
     def __repr__(self):
         return f'<Ingredient ingredient label={self.label_id} title id={self.title_id}>'
 
+class IngredientData(db.Model):
+    """Data on obscure ingredients"""
 
-class BrandFood(db.Model):
-    __tablename__ = 'brand_foods'
+    __tablename__ = 'ingredients_data'
 
-    brand_food_id = db.Column(db.Integer,
-                        autoincrement=True,
-                        primary_key=True)
-    food_id = db.Column(db.Integer,
-                        db.ForeignKey('foods.food_id'))
     ingredient_id = db.Column(db.Integer,
-                        db.ForeignKey('brands.brand_id'))
-
-
-
-class Brand(db.Model):
-    """A brand of pet food."""
-
-    __tablename__ = 'brands'
-
-    brand_id = db.Column(db.Integer,
-                        autoincrement=True,
+                        db.ForeignKey('ingredients.ingredient_id'),
                         primary_key=True)
-    brand_name = db.Column(db.String)
-    food_id = db.Column(db.Integer,
-                        db.ForeignKey('foods.food_id'))
+    true_name = db.Column(db.String)
 
     def __repr__(self):
-        return f'<Brand name={self.name} brand id={self.brand_id}>'
+        return f'<IngredientData ingredient_id={self.ingredient_id} True name={self.true_name}>'
+
+def connect_to_db(flask_app, db_uri="postgresql:///pet_food", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = False
+    #if this is set to False instead of echo, terminal will not show huge long loading data
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+if __name__ == "__main__":
+    from server import app
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+    connect_to_db(app)
+
+#relationships:
+#a brand can have many foods, a food can have many brands (many to many)
+#ie: how every brand has a generic "chicken dog food"
+#ingredients to food (many to many)
+#ie: this food has chicken, corn & soy AND chicken is in many different foods
+#ingredients to ingredient data (one to many)
+
 
 
 # class Filler(db.Model):
@@ -105,7 +137,6 @@ class Brand(db.Model):
 
 #     def __repr__(self):
 #         return f'<Filler filler_id={self.filler_id} common name={self.common_name}>'
-
 
 # class TitleIngredient(db.Model):
 #     """A title ingredient"""
@@ -126,37 +157,10 @@ class Brand(db.Model):
 #     def __repr__(self):
 #         return f'<Title title_id={self.title_id} ingredient 1 name={self.ing_1_name}>'
 
-class SubtitleIngredient(db.Model):
-    """A subtitle ingredient"""
-
-    __tablename__ = 'subtitle_ingredients'
-
-    sub_id = db.Column(db.Integer,
-                        db.ForeignKey('ingredients.ingredient_id'),
-                        primary_key=True)
-    sub_title = db.Column(db.String)
-
-    def __repr__(self):
-        return f'<Subtitle sub_id={self.sub_id} ingredient 1 name={self.ing_1_name}>'
-
-
-def connect_to_db(flask_app, db_uri="postgresql:///pet_food", echo=True):
-    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
-    flask_app.config["SQLALCHEMY_ECHO"] = False
-    #if this is set to False instead of echo, terminal will not show huge long loading data
-    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    db.app = flask_app
-    db.init_app(flask_app)
-
-    print("Connected to the db!")
-
-
-if __name__ == "__main__":
-    from server import app
-
-    # Call connect_to_db(app, echo=False) if your program output gets
-    # too annoying; this will tell SQLAlchemy not to print out every
-    # query it executes.
-
-    connect_to_db(app)
+#This used to be in the Ingredient class, now removed
+    # title_id = db.Column(db.Integer,i
+    #                     autoincrement=True)
+    # sub_id = db.Column(db.Integer,
+    #                     autoincrement=True)
+    # fillers = db.relationship('Filler', backref='ingredients')
+    # title_ingredients = db.relationship('TitleIngredient', backref='ingredients')
