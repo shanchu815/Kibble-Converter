af9f804423cd1445fb6534b7c7ff9de698892279
Shannon <shanchu815@gmail.com>
2021-12-14 04:12:09 +0000

    seed_database.py started

---
+++ seed_database.py
@@ -0,0 +1,13 @@
+''' Automates all database functions.'''
+
+import os
+import json
+import crud
+import model
+import server
+
+os.system('dropdb pet_food')
+os.system('createdb pet_food')
+
+model.connect_to_db(server.app)
+model.db.create_all()
