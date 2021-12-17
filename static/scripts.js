1edb092d24e64abf879b84ccb7d1e68d41176fbf
Shannon <shanchu815@gmail.com>
2021-12-17 00:03:25 +0000

    forgot to track calc and scripts

---
+++ static/scripts.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const firstForm = document.getElementById(calc1);
+const secondForm = document.getElementById(calc2);
+
+function trueProteinContent(a, b) {
+    return (a/(100-b)) * 100;
+}
+
+function firstCalculator(evt){
+    evt.preventDefault();
+    const crudeProtein = parseInt(firstForm['crude-protein'].value);
+    const moisture = parseInt(firstForm['moisture'].value)
+    const result = trueProteinContent(crudeProtein, moisture);
+    document.getElementById('true-protein').innerHTML = `${result}`;
+}
+
+function secondCalculator(evt){
+    evt.preventDefault();
+    const crudeProtein = parseInt(secondForm['crude-protein'].value);
+    const moisture = parseInt(secondForm['moisture'].value)
+    const result = trueProteinContent(crudeProtein, moisture);
+    document.getElementById('true-protein2').innerHTML = `${result}`;
+}
+
+firstForm.addEventListener('submit', firstCalculator);
+secondForm.addEventListener('submit', secondCalculator);
