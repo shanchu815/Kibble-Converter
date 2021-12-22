77bbca13d03a722126426b5bbe94f775651997b9
Shannon <shanchu815@gmail.com>
2021-12-22 22:04:46 +0000

    removed some unnecessary notes from server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ app.secret_key = 'W33d1sl33t1845!'
 @app.route('/')
 def start_here():
     """Home page."""
-    
+
     return render_template("homepage.html")
 
 @app.route("/disclaimer")
@@ -62,35 +62,13 @@ def save_choices():
 def show_result(food_id):
     """Displays the choices as a result page"""
     
-    if food_id not in pet_food:
+    if food_id not in db.pet_food:
         return "Invalid id"
     else:
-        return render_template("results.html", ingredients = pet_food[food_id])
+        return render_template("results.html", ingredients = db.pet_food[food_id])
+#ask Sean how do i call the pet_food db here
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads"
     # our web app if we change the code.
     app.run(debug=True, host="0.0.0.0")
-
-    # cer = f"{ing_data[cereal]['name']}: {ing_data[cereal]['effect']}, {ing_data[cereal]['details']}"
-    # oth = f"{ing_data[other]['name']}: {ing_data[other]['effect']}, {ing_data[other]['details']}"
-    # pro = f"{ing_data[protein]['name']}: {ing_data[protein]['effect']}, {ing_data[protein]['details']}"
-    # pres = f"{ing_data[preserv]['name']}: {ing_data[preserv]['effect']}, {ing_data[preserv]['details']}"
-
-    # ings = []
-    # if title_2 != 2.0:
-    #     ings.append(title_2)
-    # if title_3 != 3.0:
-    #     ings.append(title_3)
-
-    # if d_key == "dinner":
-    #     description = f"The product contains at least 25% {title_1} up to a maximum of 95%. Not counting water content, the minimum is 10%. Other named ingredients: {ings} must be at least 3% each."
-    # else:
-    #     description = f"The product contains at least {contents[d_key]['ing_min']}% {title_1}."
-
-
-beef/deer	none	    This is 95% beef/deer minimum.							
-beef/deer	with/dinner	This is 25-95% beef/deer. Ingredients named afterwards make up at least 3% each.							
-beef/deer	flavor	    This is 0% actual beef/deer. There may be stock or some other substitute used.		
-
-https://gyazo.com/a97f69d97d959b6fffd2f550ebf294c9
