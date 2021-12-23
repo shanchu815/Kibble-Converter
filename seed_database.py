da0bacbc4483e817dcac23b658b1d3d1327a43dd
Shannon <shanchu815@gmail.com>
2021-12-23 00:34:08 +0000

    data.json file editted a bit

--- seed_database.py
+++ seed_database.py
@@ -13,20 +13,10 @@ model.connect_to_db(server.app)
 model.db.create_all()
 
 
-data = data.json()
+with open('data/data.json') as f:
+    ingredient_data = json.loads(f.read())
 
-for d in data:
-    i = Ingredient(name=d.name, effect=)
+for category in ingredient_data:
+    
 
-#add ingredient data to ingredients table so it's one big table
-#they will have NUll descriptors, but there will now be an effect column in Ingredients
-#effect will be NULL for normal ingredients
-#min/max percentage will be condensed into the details column for normal ingredients
-
-#change data.json so seed_database.py can read & convert it into Ingredients table
-
-#radio buttons dont allow multiple select - change the form html
-#adjust the values of the form options to match the new classes
-
-#add new key to Food class (STR) to store the random_id
-#make new function in crud.py to get a Food object by its random_id
+#ask Sean how i should split up ingredient_data into the separate tables
