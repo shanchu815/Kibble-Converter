77394282fb16b83e753d0af08ebb64f2e0b032e5
Shannon <shanchu815@gmail.com>
2021-12-24 00:45:28 +0000

    some notes made

--- static/result.js
+++ static/result.js
@@ -1,10 +1,16 @@
 'use strict';
 
-const fillOut = ingredients;
 
-fillOut.addEventListener('submit', displayResults);
+function process(ingredients) {
 
-function displayResults(evt){
-
-    
 }
+
+const display = document.getElementById("display");
+
+// so if i am passing ingredients (a python object stored in a psql database) to an html page
+// & i want my script to take in ingredients, read its attributes and use those attributes to 
+// get the database information via my crud.py functions
+// how would i do that?
+
+// how would i get ingredients.ingredient_id in js & pass it into get_ingredient_by_id()
+// so i can get the exact ingredient details associated with that particular ingredient
