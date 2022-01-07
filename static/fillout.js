e78d753a1e4faf30725857623a8e158e82f0f0db
Shannon <shanchu815@gmail.com>
2022-01-07 02:02:37 +0000

    post sean adivising 1-6-22

--- static/fillout.js
+++ static/fillout.js
@@ -1,12 +1,24 @@
 'use strict';
 
-let radioForm = document.getElementById("others");
-let radios = radioForm.getElementsByTagName("input");
+// cannot uncheck radio buttons one by one
+// instead, add a clear button to each category of radio buttons
 
-for(i = 0; i < radios.length; i++) {
-    radios[i].onclick = function(e) {
-        if(e.ctrlKey) {
-        this.checked = false;
-        }
-    }
+document.getElementById('clear-grains').onclick = function() {
+    let grainRadio = document.querySelector('input[type=radio][name=language]:checked');
+    grainRadio.checked = false;
+}
+
+document.getElementById('clear-additives').onclick = function() {
+    let additiveRadio = document.querySelector('input[type=radio][name=language]:checked');
+    additiveRadio.checked = false;
+}
+
+document.getElementById('clear-proteins').onclick = function() {
+    let proteinRadio = document.querySelector('input[type=radio][name=language]:checked');
+    proteinRadio.checked = false;
+}
+
+document.getElementById('clear-preservatives').onclick = function() {
+    let preservativeRadio = document.querySelector('input[type=radio][name=language]:checked');
+    preservativeRadio.checked = false;
 }
