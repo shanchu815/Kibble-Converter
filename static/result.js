5f75e2cb7da6c334d459f2284e23db89ba68c881
Shannon <shanchu815@gmail.com>
2022-01-13 23:49:27 +0000

    changed result.js due to aws

--- static/result.js
+++ static/result.js
@@ -4,9 +4,13 @@ const share = document.getElementById("share-link");
 
 function copyToClipboard(evt) {
     evt.preventDefault();
-    const id =  document.getElementById("display").dataset.id;
-    navigator.clipboard.writeText(`http://localhost:5000/results/${id}`);//TODO update to static url
+    // const id =  document.getElementById("display").dataset.id;
+    navigator.clipboard.writeText(location.href);//TODO update to static url 
+    // (Don't update to static url) - didn't cover changing domain names yet
     alert("URL Copied.");
 }
 
 share.addEventListener('click', copyToClipboard);
+
+// `http://localhost:5000/results/${id}` -> location.href
+//`http://3.91.73.97/results/${id}` is the instance version (if location.href doesn't work, try this)
